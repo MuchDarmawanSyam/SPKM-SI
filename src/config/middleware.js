c327a695562286f7237e0f189b395636fb5b74e6
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2022-10-14 18:22:54 +0900

    Menambahkan file' middleware.js'.

---
+++ src/config/middleware.js
@@ -0,0 +1,23 @@
+// Middleware Custom.
+// File ini berfungsi sebagai middleware yang akan memproteksi pemanggilan controller-
+// -dari router sebelum dijalankan.
+
+module.exports = {
+    isLogin(req, res, next){
+        if(req.session.loggedin === true){
+            next();
+            return;
+        } else {
+            req.session.destroy(function(err){
+                res.redirect('/login');
+            })
+        }
+    },
+    isLogout(req, res, next){
+        if(req.session.loggedin !== true){
+            next();
+            return;
+        }
+        res.redirect('/');
+    }
+};
