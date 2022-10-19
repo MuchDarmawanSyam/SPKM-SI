119fdc89183d4ef3ac0c846393c766521ed0c52b
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2022-10-19 17:18:50 +0900

    Menambahkan asset-asset front-end. Mengubah link dan script menjadi offline

--- src/controllers/controller-home.js
+++ src/controllers/controller-home.js
@@ -1,5 +1,8 @@
 module.exports = {
     home(req, res){
+        // Mencoba disini untuk pengecekan lvl akun yg login
+        // Kemudian pisahkan render masing-masing dengan if else
+
         res.render("home", {
             url: 'http://localhost:3030/',
             userName:  req.session.nim,
