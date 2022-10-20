bc74284a1629d97193deb2be732d21375ed5c7e7
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2022-10-20 21:48:15 +0900

    Memperbarui file 'backup_base.sql'.

--- src/config/backup_base.sql
+++ src/config/backup_base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 13, 2022 at 11:20 AM
+-- Generation Time: Oct 20, 2022 at 02:46 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
