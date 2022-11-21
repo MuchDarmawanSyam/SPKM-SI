fa8ec62f318925ec08852feddeb6251c0a7a7c41
Much Darmawan Iriansyah Syam <mchdarmawan@gmail.com>
2022-11-21 19:59:49 +0900

    Memperbaiki logika router akun mhs

--- src/routes/router-akun-mahasiswa.js
+++ src/routes/router-akun-mahasiswa.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 const akunMhsController = require('../controllers').akunMahasiswa;
 const verifyUser = require('../config/middleware');
-const { akunMahasiswa } = require('../controllers');
 
 router.get('/', verifyUser.isLogin, akunMhsController.showAkunMhs);
 router.post('/detail', verifyUser.isLogin, akunMhsController.detailAkunMhs);
-router.post('/reset', verifyUser.isLogin, akunMahasiswa.resetPassAkunMhs);
+router.post('/reset', verifyUser.isLogin, akunMhsController.resetPassAkunMhs);
 
 module.exports = router;
